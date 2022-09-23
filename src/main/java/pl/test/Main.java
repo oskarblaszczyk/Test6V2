7ad215e0afa698da6c1b57c4f19a1d66b77bcaa8
oskarblaszczyk <oskar.blaszczyk@me.com>
2022-09-23 16:49:46 +0200

    powerOfN method cleaning

--- src/main/java/pl/test/Main.java
+++ src/main/java/pl/test/Main.java
@@ -2,11 +2,19 @@ package pl.test;
 
 import pl.test.zadanie2.PowerOfN;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-    PowerOfN pofn = new PowerOfN();
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test6?useSSL=false&serverTimezone=UTC",
+                "root",
+                "root"
+        );
+
+    PowerOfN pofn = new PowerOfN(conn);
 
         System.out.println(pofn.getPowerOfN(81));
 
