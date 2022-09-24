d999d5f24b1ee890e43c190f3794560f62590ae9
oskarblaszczyk <oskar.blaszczyk@me.com>
2022-09-24 08:26:45 +0200

    searchFiles method modification

--- src/main/java/pl/test/zadanie3/model/WorkspaceFile.java
+++ src/main/java/pl/test/zadanie3/model/WorkspaceFile.java
@@ -1,6 +1,7 @@
 package pl.test.zadanie3.model;
 
 
+import java.nio.file.attribute.FileTime;
 import java.time.LocalDate;
 
 public class WorkspaceFile {
@@ -8,10 +9,14 @@ public class WorkspaceFile {
     private String fileName;
     private String path;
     private long size;
-    private LocalDate created;
-    private LocalDate lastModified;
+    private FileTime created;
+    private FileTime lastModified;
 
-
-
-
+    public WorkspaceFile(String fileName, String path, long size, FileTime created, FileTime lastModified) {
+        this.fileName = fileName;
+        this.path = path;
+        this.size = size;
+        this.created = created;
+        this.lastModified = lastModified;
+    }
 }
--- src/main/java/pl/test/zadanie3/service/Exercise3Service.java
+++ src/main/java/pl/test/zadanie3/service/Exercise3Service.java
@@ -11,26 +11,40 @@ nastepnie napisz metode ktora na podstawie tego co jest w bazie znajdzie:
  */
 
 
-import java.io.File;
+import pl.test.zadanie3.dao.FileDaoImpl;
+import pl.test.zadanie3.model.WorkspaceFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+
 
 public class Exercise3Service {
     private Path path;
+    private FileDaoImpl fileDao;
 
-    public Exercise3Service(Path path) {
-        this.path = path;
+    public Exercise3Service(String path, FileDaoImpl fileDao) {
+        this.path = Paths.get(path);
+        this.fileDao = fileDao;
     }
 
-    private void searchFiles(File file){
-        for (File listFile : file.listFiles()) {
-            if(listFile.isDirectory()){
-                searchFiles(listFile);
-            }else{
-
+    private void searchFiles(Path path) throws IOException {
+        BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
+        if (attr.isDirectory()) {
+            for (Path p : path) {
+                searchFiles(p);
             }
-
+        } else if (path.endsWith("java")) {
+            fileDao.save(new WorkspaceFile(
+                    attr.toString(),
+                    path.toString(),
+                    attr.size(),
+                    attr.creationTime(),
+                    attr.lastModifiedTime()
+            ));
         }
-
 
     }
 
